d4da9c16487d13d24c8ee220c8de42f599e68075
Elvin Cervania <ejcervania@gmail.com>
2024-07-18 10:43:32 +0800

    First files

--- JavaScript/AdULabs.js
+++
@@ -1,7 +0,0 @@
-function brgr_open(){
-    document.getElementById("rightMenu").style.display = "block";
-}
-
-function brgr_close(){
-    document.getElementById("rightMenu").style.display = "none";
-}
